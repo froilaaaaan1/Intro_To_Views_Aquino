1347da9b1c858949e29115d123c9ee563596ad90
froilan <froilanaquino1@gmail.com>
2023-11-06 15:35:46 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Intro_To_Views_Aquino"
+include(":app")
