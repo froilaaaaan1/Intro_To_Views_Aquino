e3d4eb9c66beb963a29bafd074f6cd7e77f45f54
froilan <froilanaquino1@gmail.com>
2023-11-07 16:51:11 +0800

    update point #6 custom action bar's color

--- app/src/main/java/com/example/intro_to_views_aquino/MainActivity.java
+++ app/src/main/java/com/example/intro_to_views_aquino/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.intro_to_views_aquino;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBarObject = getSupportActionBar();
+        if(actionBarObject != null) {
+            actionBarObject.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.action)));
+        }
     }
 }
